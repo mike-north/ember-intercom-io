8a3e0457c42b216ac241fcb32f224b452934e873
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2017-02-21 15:06:37 +0700

    Added more API methods to mirror intercom

--- addon/services/intercom.js
+++ addon/services/intercom.js
@@ -6,13 +6,194 @@ const {
   merge,
   Service,
   computed,
+  observer,
   assert,
+  run,
   run: { scheduleOnce },
-  Evented
+  Evented,
+  RSVP: { Promise },
+  String: { underscore },
+  typeOf
 } = Ember;
+
+const { keys } = Object;
+
+function normalizeIntercomMetadata(data) {
+  let result = {};
+  keys(data).forEach((key) => {
+    if (typeOf(data[key]) === 'object') {
+      result[underscore(key)] = normalizeIntercomMetadata(data[key])
+    }else{
+      result[underscore(key)] = data[key]
+    }
+  })
+  return result;
+}
 
 export default Service.extend(Evented, {
   api: intercom,
+
+  /**
+   * Indicates the open state of the Intercom panel
+   *
+   * @public
+   * @type {Boolean}
+   */
+  isOpen: false,
+
+  /**
+   * Reports the number of unread messages
+   *
+   * @public
+   * @type {Number}
+   */
+  unreadCount: 0,
+
+  /**
+   * If true, will automatically update intercom when changes to user object are made.
+   *
+   * @type {Boolean}
+   */
+  autoUpdate: true,
+
+  user: {
+    email: null,
+    name: null,
+    createdAt: null,
+  },
+
+  boot(config = {}) {
+    this._addEventHandlers();
+    this._callIntercomMethod('boot', normalizeIntercomMetadata(config));
+  },
+
+  update(config = {}) {
+    this._callIntercomMethod('update', normalizeIntercomMetadata(config));
+  },
+
+  shutdown() {
+    this._callIntercomMethod('shutdown');
+  },
+
+  show() {
+    return this._wrapIntercomCallInPromise('show', 'show');
+  },
+
+  hide() {
+    return this._wrapIntercomCallInPromise('hide', 'hide');
+  },
+
+  toggleOpen() {
+    this.get('isOpen') ? this.hide() : this.show();
+  },
+
+  /**
+   * Opens the message window with the message list visible.
+   *
+   * @public
+   * @return {Promise}
+   */
+  showMessages() {
+    return this._wrapIntercomCallInPromise('showMessages', 'show');
+  },
+
+  /**
+   * Opens the message window with the new message view.
+   *
+   * @public
+   * @return {Promise}
+   */
+  showNewMessage(initialText) {
+    return this._wrapIntercomCallInPromise('showNewMessage', 'show', initialText);
+  },
+
+  /**
+   * You can submit an event using the trackEvent method.
+   * This will associate the event with the currently logged in user and
+   * send it to Intercom.
+   *
+   * The final parameter is a map that can be used to send optional
+   * metadata about the event.
+   *
+   * @param {String} eventName
+   * @param {Object} metadata
+   * @public
+   */
+  trackEvent() {
+    this._callIntercomMethod('trackEvent', ...arguments);
+  },
+
+  /**
+   * A visitor is someone who goes to your site but does not use the messenger.
+   * You can track these visitors via the visitor user_id. This user_id
+   * can be used to retrieve the visitor or lead through the REST API.
+   *
+   * @public
+   * @return {String} The visitor ID
+   */
+  getVisitorId() {
+    return this.get('api')('getVisitorId');
+  },
+
+  start(bootConfig = {}) {
+    let config = merge(get(this, '_intercomBootConfig'), bootConfig);
+    return this.boot(config);
+  },
+
+  stop() {
+    return this.shutdown();
+  },
+
+  _onHide() {
+    this.set('isOpen', false);
+    this.trigger('hide');
+  },
+
+  _onShow() {
+    this.set('isOpen', true);
+    this.trigger('show');
+  },
+
+  _onUnreadCountChange(count) {
+    this.set('unreadCount', parseInt(count, 10));
+  },
+
+  _addEventHandlers() {
+    if (this._hasEventHandlers) {
+      return;
+    }
+    this._callIntercomMethod('onHide', () => run(this, this._onHide));
+    this._callIntercomMethod('onShow', () => run(this, this._onShow));
+    this._callIntercomMethod('onUnreadCountChange', (count) => run(this, this._onUnreadCountChange, count));
+    this._hasEventHandlers = true;
+  },
+
+  _wrapIntercomCallInPromise(intercomMethod, eventName, ...args) {
+    return new Promise((resolve) => {
+      let isOpen = this.get('isOpen');
+      if (eventName === 'show' && isOpen
+          || eventName === 'hide' && !isOpen) {
+        run.next(this, resolve);
+      } else {
+        this.one(eventName, resolve);
+      }
+      this._callIntercomMethod(intercomMethod, ...args);
+    });
+  },
+
+  _callIntercomMethod(methodName, ...args) {
+    scheduleOnce('afterRender', () => {
+      let intercom = this.get('api');
+      debugger
+      intercom[methodName].apply(intercom, args);
+    });
+  },
+
+  userDataDidChange: observer('user.@each', function() {
+    if (this.get('autoUpdate')) {
+      this.update(this.get('user'));
+    }
+  }),
 
   _userNameProp: computed('config.userProperties.nameProp', function() {
     return get(this, `user.${get(this, 'config.userProperties.nameProp')}`);
@@ -25,11 +206,6 @@ export default Service.extend(Evented, {
   _userCreatedAtProp: computed('config.userProperties.createdAtProp', function() {
     return get(this, `user.${get(this, 'config.userProperties.createdAtProp')}`);
   }),
-
-  user: {
-    name: null,
-    email: null
-  },
 
   _hasUserContext: computed('user', '_userNameProp', '_userEmailProp', '_userCreatedAtProp', function() {
     return !!get(this, 'user') &&
@@ -57,17 +233,4 @@ export default Service.extend(Evented, {
 
     return obj;
   }),
-
-  start(bootConfig = {}) {
-    let _bootConfig = merge(get(this, '_intercomBootConfig'), bootConfig);
-    scheduleOnce('afterRender', () => this.get('api')('boot', _bootConfig));
-  },
-
-  stop() {
-    scheduleOnce('afterRender', () => this.get('api')('shutdown'));
-  },
-
-  update(properties = {}) {
-    scheduleOnce('afterRender', () => this.get('api')('update', properties));
-  }
 });
